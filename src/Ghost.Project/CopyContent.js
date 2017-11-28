3eeff2803a28d161c3696f2cb897876bb23b505b
Kirsten Kluge <live@kirkone.de>
2017-11-28 23:01:22 +0100

    cleanup

--- src/Ghost.Project/CopyContent.js
+++ src/Ghost.Project/CopyContent.js
@@ -4,6 +4,7 @@ let jobs = {
     "node_modules/ghost/content/themes": "content/themes",
 
     // This is a hack as long this is not solved: https://github.com/TryGhost/Ghost/issues/8754
+    // "node_modules/ghost-azure-storage/index.js": "content/adapters/storage/ghost-azure-storage.js"
     "node_modules/ghost-azure-storage/index.js": "node_modules/ghost/core/server/adapters/storage/ghost-azure-storage.js"
 };
 
