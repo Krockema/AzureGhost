82ab26c76fb2ebff01e7fcf2ca2dc7954f6a12f3
Kirsten Kluge <live@kirkone.de>
2017-11-28 23:34:11 +0100

    added stuff

--- src/Ghost.Project/index.js
+++ src/Ghost.Project/index.js
@@ -1,5 +1,21 @@
 process.env.server__port = process.env.PORT;
-var ghost = require('ghost');
+let ghost = require('ghost');
+
+let appInsightsKey = process.env.APPINSIGHTS_INSTRUMENTATION_KEY;
+
+if (appInsightsKey) {
+    let appInsights = require("applicationinsights");
+
+    appInsights.setup(appInsightsKey)
+        .setAutoDependencyCorrelation(true)
+        .setAutoCollectRequests(true)
+        .setAutoCollectPerformance(true)
+        .setAutoCollectExceptions(true)
+        .setAutoCollectDependencies(true)
+        .setAutoCollectConsole(true)
+        .setUseDiskRetryCaching(true)
+        .start();
+}
 
 ghost().then(function (ghostServer) {
     ghostServer.start();
